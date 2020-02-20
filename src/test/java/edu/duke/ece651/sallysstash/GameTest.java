f8687a79b3bd37e210f52a2024a6073c242a0f86
yy <yueyang0115@outlook.com>
2020-02-20 09:10:02 -0500

    add testcases

--- src/test/java/edu/duke/ece651/sallysstash/GameTest.java
+++ src/test/java/edu/duke/ece651/sallysstash/GameTest.java
@@ -4,19 +4,68 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 import org.junit.jupiter.api.Test;
 
 public class GameTest {
-  //@Test
   /*
-  public void test_Game() {
-    String filename = "./src/test/resources/input.txt";
+  @Test
+  public void test_Player() {
+    String filename = "./src/test/resources/player.txt";
     try {
       // String input[] = " ";
       System.setIn(new FileInputStream(filename));
-      Game.main(System.in);
     } catch (FileNotFoundException e) {
       System.out.println("File not exist");
     }
+    Board board_A = new Board();
+    Board board_B = new Board();
+    Scanner scanner = new Scanner(System.in);
+    Player player_A = Game.init(scanner, board_A, 'A');
+    Player player_B = Game.init(scanner, board_B, 'B');
+    player_A.putAllStack('A', 'B');
+    player_B.putAllStack('B', 'A');
+    int total = 4;
+    while ((player_A.CountHitted() != total) && (player_B.CountHitted() != total)) {
+      player_A.ActionSelect(board_B, 'A', 'B');
+      if (player_B.CountHitted() == total) {
+        myUtils.IS_WIN('A');
+        break;
+      }
+      player_B.ActionSelect(board_A, 'B', 'A');
+      if (player_A.CountHitted() == total) {
+        myUtils.IS_WIN('B');
+      }
+    }
     }*/
+
+  @Test
+  public void test_Robot() {
+    String filename = "./src/test/resources/robot.txt";
+    try {
+      // String input[] = " ";
+      System.setIn(new FileInputStream(filename));
+    } catch (FileNotFoundException e) {
+      System.out.println("File not exist");
+    }
+    Board board_A = new Board();
+    Board board_B = new Board();
+    Scanner scanner = new Scanner(System.in);
+    Player player_A = Game.init(scanner, board_A, 'A');
+    Player player_B = Game.init(scanner, board_B, 'B');
+    player_A.putAllStack('A', 'B');
+    player_B.putAllStack('B', 'A');
+    int total = 43;
+    while ((player_A.CountHitted() != total) && (player_B.CountHitted() != total)) {
+      player_A.ActionSelect(board_B, 'A', 'B');
+      if (player_B.CountHitted() == total) {
+        myUtils.IS_WIN('A');
+        break;
+      }
+      player_B.ActionSelect(board_A, 'B', 'A');
+      if (player_A.CountHitted() == total) {
+        myUtils.IS_WIN('B');
+      }
+    }
+  }
 }

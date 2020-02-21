9b480a7c1163cd7f61c40f4e1e093a513e8dcbf7
yy <yueyang0115@outlook.com>
2020-02-20 19:06:13 -0500

    deliver finalize version2

--- src/test/java/edu/duke/ece651/sallysstash/GameTest.java
+++ src/test/java/edu/duke/ece651/sallysstash/GameTest.java
@@ -4,41 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
 import org.junit.jupiter.api.Test;
 
 public class GameTest {
-  /* @Test
-  public void test_Player() {
-    System.out.println("test for player");
-    String filename = "./src/test/resources/player.txt";
-    try {
-      // String input[] = " ";
-      System.setIn(new FileInputStream(filename));
-    } catch (FileNotFoundException e) {
-      System.out.println("File not exist");
-    }
-    Board board_A = new Board();
-    Board board_B = new Board();
-    Scanner scanner = new Scanner(System.in);
-    Player player_A = new Player(scanner, board_A);
-    Player player_B = new Player(scanner, board_B);
-    player_A.putAllStack('A', 'B');
-    player_B.putAllStack('B', 'A');
-    int total = 4;
-    while ((player_A.CountHitted() != total) && (player_B.CountHitted() != total)) {
-      player_A.ActionSelect(board_B, 'A', 'B');
-      if (player_B.CountHitted() == total) {
-        myUtils.IS_WIN('A');
-        break;
-      }
-      player_B.ActionSelect(board_A, 'B', 'A');
-      if (player_A.CountHitted() == total) {
-        myUtils.IS_WIN('B');
-      }
-    }
-    }*/
-
   @Test
   public void test_robot() throws FileNotFoundException {
     String filename = "./src/test/resources/robot.txt";
